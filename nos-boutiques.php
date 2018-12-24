745dd34a3f1de4f680fdd0f2f3a58910015d56d0
OliverMute <olivieralexis@yahoo.com>
2018-12-24 18:45:25 +0100

    initial commit

---
+++ nos-boutiques.php
@@ -0,0 +1,318 @@
+<?php 
+	require_once("includes/header.php");
+ ?>
+
+	<div class="container-fluid small-link">
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<a class="d-inline-block" href="index.php">Lovin Sweet</a>
+				<p class="d-inline-block"> > Nos boutiques </p>
+			</div>
+		</div>
+	</div>
+ 	<div class="container-fluid" id="main-container">
+	<section class="container" id="iframe">
+		<div class="row text-center">
+			<div class="col-md-4 offset-md-7">
+				<h2 class="button-style-1 display-4 text-center ">
+				Nos<br> boutiques</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d83998.75769450165!2d2.2770201074432013!3d48.85895068103847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2sParis!5e0!3m2!1sen!2sfr!4v1545603836878" width="1300" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</div>
+		</div>
+	</section>
+
+
+	<!-- Section 1 -->
+	<section class="container" id="nos-boutiques-paris">
+		<div class="row ">
+			<div class="col-md-12 text-center">
+				<h2 class="button-style-1 display-4 text-center ">
+					Nos boutiques sur Paris
+				</h2>
+			</div>
+		</div>
+
+
+		<!-- Row 1 -->
+		<div class="row">
+			<div class="col-md-3 ">
+				<ul>
+					<div class="block-boutique">
+						<li>
+							<h3 class="text-center">
+								Lovin Sweet<br>Bastille
+							</h3>
+						</li>
+						<li class="offset-md-2">
+							10, rue Saint Antoine<br>
+							75004 Paris<br>
+							01 53 01 91 91
+						</li>
+					</div>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<ul>
+					<div class="block-boutique">
+						<li>
+							<h3 class="text-center">
+								Lovin Sweet<br>Bastille
+							</h3>
+						</li>
+						<li class="offset-md-2">
+							10, rue Saint Antoine<br>
+							75004 Paris<br>
+							01 53 01 91 91
+						</li>
+					</div>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<ul>
+					<div class="block-boutique">
+						<li>
+							<h3 class="text-center">
+								Lovin Sweet<br>Bastille
+							</h3>
+						</li>
+						<li class="offset-md-2">
+							10, rue Saint Antoine<br>
+							75004 Paris<br>
+							01 53 01 91 91
+						</li>
+					</div>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<ul>
+					<div class="block-boutique">
+						<li>
+							<h3 class="text-center">
+								Lovin Sweet<br>Bastille
+							</h3>
+						</li>
+						<li class="offset-md-2">
+							10, rue Saint Antoine<br>
+							75004 Paris<br>
+							01 53 01 91 91
+						</li>
+					</div>
+				</ul>
+			</div>
+		</div>
+
+
+
+		<!-- Row 2 -->
+		<div class="row">
+			<div class="col-md-3 ">
+				<ul>
+					<div class="block-boutique">
+						<li>
+							<h3 class="text-center">
+								Lovin Sweet<br>Bastille
+							</h3>
+						</li>
+						<li class="offset-md-2">
+							10, rue Saint Antoine<br>
+							75004 Paris<br>
+							01 53 01 91 91
+						</li>
+					</div>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<ul>
+					<div class="block-boutique">
+						<li>
+							<h3 class="text-center">
+								Lovin Sweet<br>Bastille
+							</h3>
+						</li>
+						<li class="offset-md-2">
+							10, rue Saint Antoine<br>
+							75004 Paris<br>
+							01 53 01 91 91
+						</li>
+					</div>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<ul>
+					<div class="block-boutique">
+						<li>
+							<h3 class="text-center">
+								Lovin Sweet<br>Bastille
+							</h3>
+						</li>
+						<li class="offset-md-2">
+							10, rue Saint Antoine<br>
+							75004 Paris<br>
+							01 53 01 91 91
+						</li>
+					</div>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<ul>
+					<div class="block-boutique">
+						<li>
+							<h3 class="text-center">
+								Lovin Sweet<br>Bastille
+							</h3>
+						</li>
+						<li class="offset-md-2">
+							10, rue Saint Antoine<br>
+							75004 Paris<br>
+							01 53 01 91 91
+						</li>
+					</div>
+				</ul>
+			</div>
+		</div>
+
+
+		<!-- Row 3 -->
+		<div class="row">
+			<div class="col-md-3 ">
+				<ul>
+					<div class="block-boutique">
+						<li>
+							<h3 class="text-center">
+								Lovin Sweet<br>Bastille
+							</h3>
+						</li>
+						<li class="offset-md-2">
+							10, rue Saint Antoine<br>
+							75004 Paris<br>
+							01 53 01 91 91
+						</li>
+					</div>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<ul>
+					<div class="block-boutique">
+						<li>
+							<h3 class="text-center">
+								Lovin Sweet<br>Bastille
+							</h3>
+						</li>
+						<li class="offset-md-2">
+							10, rue Saint Antoine<br>
+							75004 Paris<br>
+							01 53 01 91 91
+						</li>
+					</div>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<ul>
+					<div class="block-boutique">
+						<li>
+							<h3 class="text-center">
+								Lovin Sweet<br>Bastille
+							</h3>
+						</li>
+						<li class="offset-md-2">
+							10, rue Saint Antoine<br>
+							75004 Paris<br>
+							01 53 01 91 91
+						</li>
+					</div>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<ul>
+					<div class="block-boutique">
+						<li>
+							<h3 class="text-center">
+								Lovin Sweet<br>Bastille
+							</h3>
+						</li>
+						<li class="offset-md-2">
+							10, rue Saint Antoine<br>
+							75004 Paris<br>
+							01 53 01 91 91
+						</li>
+					</div>
+				</ul>
+			</div>
+		</div>
+	</section>
+
+
+
+
+	
+	<!-- Section 2 -->
+	<section class="container" id="nos-boutiques-france">
+		<div class="row ">
+			<div class="col-md-12 text-center">
+				<h2 class="button-style-1 display-4 display-4 text-center">
+					Nos autres boutiques
+				</h2>
+			</div>
+		</div>
+
+
+		<!-- Row 1 -->
+		<div class="row">
+			<div class="col-md-3 ">
+				<ul>
+					<div class="block-boutique">
+						<li>
+							<h3 class="text-center">
+								Lovin Sweet<br>Bastille
+							</h3>
+						</li>
+						<li class="offset-md-2">
+							10, rue Saint Antoine<br>
+							75004 Paris<br>
+							01 53 01 91 91
+						</li>
+					</div>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<ul>
+					<div class="block-boutique">
+						<li>
+							<h3 class="text-center">
+								Lovin Sweet<br>Bastille
+							</h3>
+						</li>
+						<li class="offset-md-2">
+							10, rue Saint Antoine<br>
+							75004 Paris<br>
+							01 53 01 91 91
+						</li>
+					</div>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<ul>
+					<div class="block-boutique">
+						<li>
+							<h3 class="text-center">
+								Lovin Sweet<br>Bastille
+							</h3>
+						</li>
+						<li class="offset-md-2">
+							10, rue Saint Antoine<br>
+							75004 Paris<br>
+							01 53 01 91 91
+						</li>
+					</div>
+				</ul>
+			</div>
+	</section>
+</div>
+
+ <?php 
+ 	require_once("includes/footer.php");
+  ?>
